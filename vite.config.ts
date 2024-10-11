75b381f3290bb32ded7377841c1ba497235e381c
Jesús Mur Fontanals <murfontanals@gmail.com>
2024-10-11 13:49:39 +0200

    First commit, basic pwa and sass config

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'Green Light, Red Light by Jesus Mur',
+      short_name: 'A game by Jesus Mur',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  })],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `@import "./styles/variables.scss";`
+      }
+    }
+  }
+})
