b13c9f4682a4c9246105080fd2c4c8e9833543a8
Jesús Mur Fontanals <murfontanals@gmail.com>
2024-10-14 16:18:10 +0200

    chore: import changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useCurrentPlayer } from '../../hooks/useCurrentPlayer';
+import { useCurrentPlayer } from '../../hooks';
 
 import './Navbar.scss';
 
